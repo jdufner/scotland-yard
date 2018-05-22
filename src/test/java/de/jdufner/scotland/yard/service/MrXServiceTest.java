1a2971f09619e512ddd13d35e544c44767032dc3
jdufner <juergen@jdufner.de>
2018-05-22 22:14:56 +0200

    added copyright and architecture documentation template

--- src/test/java/de/jdufner/scotland/yard/service/MrXServiceTest.java
+++ src/test/java/de/jdufner/scotland/yard/service/MrXServiceTest.java
@@ -1,4 +1,27 @@
+/*
+ * Scotland Yard is a simulation of the board game.
+ * Copyright (C) 2008-2018  Juergen Dufner
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package de.jdufner.scotland.yard.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 import de.jdufner.scotland.yard.model.PositionUndTickets;
 import de.jdufner.scotland.yard.model.spiel.Spiel;
@@ -8,10 +31,6 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
 
 /**
  * @author Jürgen DUfner
